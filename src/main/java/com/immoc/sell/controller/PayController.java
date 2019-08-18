3a039c33d8929dbae400ece0b03e975ec520b441
wangabeng <wangabeng@163.com>
2019-08-19 01:14:52 +0800

    update

--- src/main/java/com/immoc/sell/controller/PayController.java
+++ src/main/java/com/immoc/sell/controller/PayController.java
@@ -43,20 +43,23 @@ public class PayController {
 		// 2 发起支付(调用统一下单API)
 		PayResponse payResponse = payService.create(orderDTO);
 
-		System.out.println("将要跳转");
+		System.out.println("将要跳转到：pay/create");
 		// 然后把统一下单返回的数据返回到前端 以供支付
 		map.put("payResponse", payResponse);
 		map.put("returnUrl", returnUrl);
 		return new ModelAndView("pay/create", map);
 	}
 	
+	// @RequestBody String notifyData
+	//	测试 http://nicedevelop.nat300.top/pay/create?orderId=1564664460087302923&returnUrl=http://nicedevelop.nat300.top/pay/notify
 	@PostMapping("/notify")
-	public void  notify (@RequestBody String notifyData) {
-		payService.notify(notifyData);
+	public ModelAndView  notify(@RequestBody String notifyData) {
+		// payService.notify(notifyData);
 		// 返回给微信处理结果（用模板引擎返回）
-		System.out.println("将要跳转到：\"pay/success\"");
-		System.out.println("notifyData：" + notifyData); // 此时已经报错 Request method 'GET' not supported
-		// return new ModelAndView("pay/success"); // 问题： 没有执行这一步
-		// return null;
+		// System.out.println("将要跳转到：\"pay/success\"");
+		System.out.println("notifyData：" + notifyData); // 正常执行
+		// System.out.println("进入/notify了");
+		return new ModelAndView("pay/success"); // 问题： 没有执行这一步
+
 	}
 }
--- src/main/resources/templates/pay/create.ftl
+++ src/main/resources/templates/pay/create.ftl
@@ -13,7 +13,7 @@ function onBridgeReady(){
 	      if(res.err_msg == "get_brand_wcpay_request:ok" ){
 	      // 使用以上方式判断前端返回,微信团队郑重提示：
 	            //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-	          location.href = "${returnUrl}"  
+	          // location.href = "${returnUrl}"  
 	      } 
 	   }); 
 	}
